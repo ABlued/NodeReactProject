70849dd3ebaecbc52aa8d62ca27b653025f95722
ablued <skwodhkswkd@naver.com>
2021-04-21 22:31:16 +0900

    <uploadpage> select 구현 완료, dropzone 구현 중

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,18 @@
 import React, {useState} from 'react'
 import {Typography, Button, Form, Input} from 'antd';
-
+import FileUpload from '../../utils/FileUpload';
 const { TextArea } = Input;
+
+const Continents = [
+    {key:1, value: "Africa"},
+    {key:2, value: "Europe"},
+    {key:3, value: "Asia"},
+    {key:4, value: "North America"},
+    {key:5, value: "South America"},
+    {key:6, value: "Australia"},
+    {key:7, value: "Antarctica"}
+]
+
 function UploadProductPage() {
     const [Title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
@@ -19,19 +30,24 @@ function UploadProductPage() {
     const priceChangeHandler = (e) => {
         setPrice(e.currentTarget.value)    
     }
+    const continentChangeHandler = (e) => {
+        setContinent(e.currentTarget.value)
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
             <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
                 <h2 level={2}>여행 상품 업로드</h2>
             </div>
         <Form>
+            <FileUpload/>
             <br/>
             <br/>
             <label>이름</label>
             <Input onChange={titleChangeHandler} value={Title}/>
             <br/>
             <br/>
-            <label>이름</label>
+            <label>설명</label>
             <TextArea onChange={descriptionChangeHandler} value={Description}/>
             <br/>
             <br/>
@@ -39,8 +55,10 @@ function UploadProductPage() {
             <Input type="number" onChange={priceChangeHandler} value={Price}/>
             <br/>
             <br/>
-            <select>
-                <option></option>
+            <select onChange={continentChangeHandler} value={Continent}>
+                {Continents.map(item => (
+                    <option key={item.key} value={item.key}>{item.value}</option>
+                ))}
             </select>
             <br/>
             <br/>
